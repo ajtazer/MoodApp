e9c7b0a15dd8ad865d5ed278246f91eacd891756
Anuj Rawat <37796611+ajtazer@users.noreply.github.com>
2023-07-09 21:04:14 +0530

    https://github.com/codingforinnovations/Django-on-Vercel/blob/main/backend/settings.py

--- MoodApp/settings.py
+++ MoodApp/settings.py
@@ -1,12 +1,11 @@
-from dotenv import load_dotenv
 from pathlib import Path
 import os
+import dj_database_url
+from decouple import config
 
 
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
@@ -75,14 +74,12 @@ WSGI_APPLICATION = 'MoodApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
@@ -118,11 +115,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = BASE_DIR / 'prod_staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATIC_ROOT = BASE_DIR / "staticfiles_build" / "static"
 
 MEDIA_URL = '/media/'
 # if DEBUG:
