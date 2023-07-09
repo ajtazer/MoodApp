8b82112cb65f75b47886ce1293e86379390d419a
TAZER <ajcoolx619@gmail.com>
2023-07-10 01:03:49 +0530

    static for site css pakka wali

--- MoodApp/wsgi.py
+++ MoodApp/wsgi.py
@@ -13,4 +13,8 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'MoodApp.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+
+if __name__ == '__main__':
+    from django.core.management import execute_from_command_line
+    execute_from_command_line()
