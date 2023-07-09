4884de132c99d5effe8a6aa299c41642421fc617
Anuj Rawat <37796611+ajtazer@users.noreply.github.com>
2023-07-09 20:11:55 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@
 echo "Building the project..."
 python3 -m pip install -r requirements.txt
 
+echo "Collect Static..."
+python3 manage.py collectstatic --noinput --clear
+
 echo "Make Migration..."
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
-
-echo "Collect Static..."
-python3 manage.py collectstatic --noinput --clear
