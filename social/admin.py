80b07a8096426ff5283784f97586d0efe48ad9ba
Anuj Rawat <37796611+ajtazer@users.noreply.github.com>
2023-08-04 22:55:32 +0530

    Update admin.py

--- social/admin.py
+++ social/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from social.models import SocialProfile
+from social.models import SocialProfile, Post
 
 admin.site.register(SocialProfile)
+admin.site.register(Post)
